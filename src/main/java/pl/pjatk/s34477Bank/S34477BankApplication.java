4da3702096ed4266764e932d41638f72195456df
Klara Słupska <klaraslupska@gmail.com>
2026-01-17 15:23:37 +0100

    commit

---
+++ src/main/java/pl/pjatk/s34477Bank/S34477BankApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s34477Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S34477BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S34477BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/s34477Bank/S34477BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s34477Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S34477BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
